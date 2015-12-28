730a41d4e2eeb3844cbfa2c1b498a3533a5e324b
toadums <pademchuk@gmail.com>
2015-12-28 10:53:10 -0800

    Remove status bar

--- index.js
+++ index.js
@@ -20,34 +20,7 @@ const TitleShape = {
   tintColor: PropTypes.string,
 };
 
-const StatusBarShape = {
-  style: PropTypes.oneOf(['light-content', 'default', ]),
-  hidden: PropTypes.bool,
-  tintColor: PropTypes.string,
-  hideAnimation: PropTypes.oneOf(['fade', 'slide', 'none', ]),
-  showAnimation: PropTypes.oneOf(['fade', 'slide', 'none', ]),
-};
-
-function customizeStatusBar(data) {
-  if (data.style) {
-    StatusBarIOS.setStyle(data.style, true);
-  }
-  const animation = data.hidden ?
-    (data.hideAnimation || NavigationBar.defaultProps.statusBar.hideAnimation) :
-    (data.showAnimation || NavigationBar.defaultProps.statusBar.showAnimation);
-
-  StatusBarIOS.setHidden(data.hidden, animation);
-}
-
 module.exports = class NavigationBar extends Component {
-  componentDidMount() {
-    customizeStatusBar(this.props.statusBar);
-  }
-
-  componentWillReceiveProps(props) {
-    customizeStatusBar(this.props.statusBar);
-  }
-
   getButtonElement(data = {}, style) {
     if (!!data.props) {
       return <View style={styles.navBarButton}>{data}</View>;
@@ -79,12 +52,8 @@ module.exports = class NavigationBar extends Component {
     const customTintColor = this.props.tintColor ?
       { backgroundColor: this.props.tintColor, } : null;
 
-    const statusBar = !this.props.statusBar.hidden ?
-      <View style={[styles.statusBar, ]} /> : null;
-
     return (
       <View style={[styles.navBarContainer, customTintColor, ]}>
-        {statusBar}
         <View style={[styles.navBar, this.props.style, ]}>
           {this.getTitleElement(this.props.title)}
           {this.getButtonElement(this.props.leftButton, { marginLeft: 8, })}
@@ -111,12 +80,6 @@ module.exports = class NavigationBar extends Component {
   }
 
   static defaultProps = {
-    statusBar: {
-      style: 'default',
-      hidden: false,
-      hideAnimation: 'slide',
-      showAnimation: 'slide',
-    },
     title: {
       title: '',
     },
